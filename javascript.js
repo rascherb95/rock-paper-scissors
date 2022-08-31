c7f1d991b2a06ab733b6f5833f39668cefcd54aa
Robert <robascherb@gmail.com>
2022-08-31 13:32:33 -0400

    Add fall-through switch for the game playing. Game is now operable

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
-let playerChoice = "initial";
+let playerChoice;
 let computerChoice;
 let result;
-const buttonsArea = document.querySelector("#buttons")
-const choiceDisplay = document.querySelector("#displayPlayerChoice")
-const playButton = document.querySelector("#playGame");
+
 const choices = ['rock', 'paper', 'scissors'];
+const buttonsArea = document.querySelector("#buttons");
+const playButton = document.querySelector("#playGame");
 
 choices.forEach(choice => {
     button = document.createElement("button");
@@ -23,6 +23,29 @@ function getComputerChoice() {
 }
 
 playButton.addEventListener('click', () => {
-    document.getElementById('displayResults').innerHTML = "You chose " + playerChoice + " and the Computer chose " + getComputerChoice(); 
+    document.getElementById('displaySelections').innerHTML = "You chose " + playerChoice + " and the Computer chose " + getComputerChoice();
+    result = playerChoice + computerChoice;
+    console.log(result);
+
+    switch (result) {
+        case "rockscissors":
+        case "paperrock":
+        case "scissorspaper":
+            document.getElementById("gameResult").innerHTML = "You won!!!!!!!!!"
+            break;
+        case "rockpaper":
+        case "paperscissors":
+        case "scissorsrock":
+            document.getElementById("gameResult").innerHTML = "You lost :(((((((("
+            break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+            document.getElementById("gameResult").innerHTML = "You tied"
+            break;
+
+
+
+    }
 
 })
