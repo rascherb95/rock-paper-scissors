71936215afd104de91edfeb2d661b09b4c1e89b1
Robert <robascherb@gmail.com>
2022-08-31 11:52:10 -0400

    Add button click functionality     and player selection display

--- javascript.js
+++ javascript.js
@@ -1,11 +1,31 @@
+var playerChoice;
+var computerChoice;
+let result;
+const buttonsArea = document.querySelector("#buttons")
+const choiceDisplay = document.querySelector("#displayPlayerChoice")
 const choices = ['rock', 'paper', 'scissors'];
 
-const createButtons = document.getElementById("buttons");
-
-for (var choice in choices) {
-    var button = document.createElement("BUTTON");
-    button.innerHTML = choices[choice];
-    createButtons.appendChild(button);
-
+function getComputerChoice() {
+    let ComputerChoice = choices[Math.floor(Math.random() * choices.length)]
+    console.log("the Computer chose " + ComputerChoice)
 }
 
+choices.forEach(choice => {
+    const button = document.createElement("button");
+    button.innerHTML = choice;
+    button.addEventListener('click', () => {
+        let playerChoice = choice;
+        console.log("you selected " + playerChoice);
+        document.getElementById("displayPlayerChoice").innerHTML = "You selected "+ playerChoice;
+        
+    });
+    buttonsArea.appendChild(button)
+})
+
+getComputerChoice ()
+
+
+function playGame(playerChoice, computerChoice) {
+    let result = playerChoice + computerChoice
+    console.log(result)
+}
