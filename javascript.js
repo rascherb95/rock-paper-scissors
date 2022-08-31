29dee9a8219e0de4512f4e6e30376abd81692877
Robert <robascherb@gmail.com>
2022-08-31 13:14:21 -0400

    error correction for playerChoice scoping

--- javascript.js
+++ javascript.js
@@ -1,31 +1,28 @@
-var playerChoice;
-var computerChoice;
+let playerChoice = "initial";
+let computerChoice;
 let result;
 const buttonsArea = document.querySelector("#buttons")
 const choiceDisplay = document.querySelector("#displayPlayerChoice")
+const playButton = document.querySelector("#playGame");
 const choices = ['rock', 'paper', 'scissors'];
 
-function getComputerChoice() {
-    let ComputerChoice = choices[Math.floor(Math.random() * choices.length)]
-    console.log("the Computer chose " + ComputerChoice)
-}
-
 choices.forEach(choice => {
-    const button = document.createElement("button");
+    button = document.createElement("button");
     button.innerHTML = choice;
     button.addEventListener('click', () => {
-        let playerChoice = choice;
-        console.log("you selected " + playerChoice);
-        document.getElementById("displayPlayerChoice").innerHTML = "You selected "+ playerChoice;
-        
+        playerChoice = choice;
+        document.getElementById("displayPlayerChoice").innerHTML = "You selected " + playerChoice;
+
     });
     buttonsArea.appendChild(button)
 })
 
-getComputerChoice ()
 
-
-function playGame(playerChoice, computerChoice) {
-    let result = playerChoice + computerChoice
-    console.log(result)
+function getComputerChoice() {
+    return computerChoice = choices[Math.floor(Math.random() * choices.length)];
 }
+
+playButton.addEventListener('click', () => {
+    document.getElementById('displayResults').innerHTML = "You chose " + playerChoice + " and the Computer chose " + getComputerChoice(); 
+
+})
